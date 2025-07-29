17e9c0a47debbb14d4018fd2e14f2808c11e2b96
Steve Tu <stevetu11@yahoo.com>
2025-07-28 19:59:39 -0700

    Print warning messages about non-1080p device widths and Debug Mode being enabled

--- app/src/main/java/com/steve1316/uma_android_automation/bot/Game.kt
+++ app/src/main/java/com/steve1316/uma_android_automation/bot/Game.kt
@@ -1106,6 +1106,8 @@ class Game(val myContext: Context) {
 
 		// Print device and version information.
 		printToLog("[INFO] Device Information: ${MediaProjectionService.displayWidth}x${MediaProjectionService.displayHeight}, DPI ${MediaProjectionService.displayDPI}")
+		if (MediaProjectionService.displayWidth != 1080) printToLog("[WARNING] ⚠️ Bot performance will be severely degraded since display width is not 1080p unless an appropriate scale is set for your device.")
+		if (debugMode) printToLog("[WARNING] ⚠️ Debug Mode is enabled. All bot operations will be significantly slower as a result.")
 		if (sharedPreferences.getString("customScale", "1.0")?.toDouble() != 1.0) printToLog("[INFO] Manual scale has been set to ${sharedPreferences.getString("customScale", "1.0")?.toDouble()}")
 		val packageInfo = myContext.packageManager.getPackageInfo(myContext.packageName, 0)
 		printToLog("[INFO] Bot version: ${packageInfo.versionName} (${packageInfo.versionCode})\n\n")
